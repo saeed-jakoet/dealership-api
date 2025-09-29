4fb24c6070955185aff7b9e1b820d703b22b6b0d
saeed-jakoet <jakoetsaeed@gmail.com>
2025-09-29 20:49:06 +0200

    domain added

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:3001",
       "http://localhost:3002",
       "http://localhost:3003",
+      "https://farauto.co.za",
     ],
     credentials: true,
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
